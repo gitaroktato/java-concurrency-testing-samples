6ed1c83ec0c6b5ce1d53cf6981283fb60fd36fe3
Oresztesz Margaritisz <oresztesz_margaritisz@epam.com>
2020-11-10 10:10:02 +0100

    Added working test

---
+++ src/main/java/Incrementor.java
@@ -0,0 +1,9 @@
+public class Incrementor {
+
+    private int value = 0;
+
+    public int increment() {
+        value++;
+        return value;
+    }
+}
---
+++ src/test/java/IncrementorTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
+public class IncrementorTest {
+
+    public static final int CYCLES = 200;
+    public static final int THREADS = 200;
+
+    @Test
+    public void testIncrement() throws InterruptedException {
+        List<Integer> results = new CopyOnWriteArrayList<>();
+        var subject = new Incrementor();
+        var barrier = new CyclicBarrier(THREADS);
+        var pool = Executors.newFixedThreadPool(THREADS);
+        for (int i = 0; i < THREADS; i++) {
+            pool.submit(() -> {
+                try {
+                    barrier.await();
+                    for (int j = 0; j < CYCLES; j++) {
+                        var result = subject.increment();
+                        results.add(result);
+                    }
+                    barrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        pool.shutdown();
+        // TODO check for timeout
+        pool.awaitTermination(10, TimeUnit.SECONDS);
+        var duplicateElements = results.stream()
+                .filter(e -> Collections.frequency(results, e) > 1)
+                .distinct()
+                .collect(Collectors.toList());
+        Assertions.assertIterableEquals(Collections.emptyList(), duplicateElements);
+    }
+
+}
