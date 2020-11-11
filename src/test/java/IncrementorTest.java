76e200517dcaec6cde0d6d4b0413a6df730839d9
Oresztesz Margaritisz <oresztesz_margaritisz@epam.com>
2020-11-11 17:22:59 +0100

    Simplified tests

--- src/test/java/IncrementorTest.java
+++ src/test/java/IncrementorTest.java
@@ -5,7 +5,9 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class IncrementorTest {
 
@@ -16,30 +18,41 @@ public class IncrementorTest {
     public void testIncrement() throws InterruptedException {
         List<Integer> results = new CopyOnWriteArrayList<>();
         var subject = new Incrementor();
-        var barrier = new CyclicBarrier(THREADS);
-        var pool = Executors.newFixedThreadPool(THREADS);
-        for (int i = 0; i < THREADS; i++) {
-            pool.submit(() -> {
-                try {
-                    barrier.await();
-                    for (int j = 0; j < CYCLES; j++) {
-                        var result = subject.increment();
-                        results.add(result);
+        runConcurrently(THREADS, CYCLES, () -> {
+            var result = subject.increment();
+            results.add(result);
+        });
+        var duplicateElements = collectDuplicates(results);
+        Assertions.assertIterableEquals(Collections.emptyList(), duplicateElements);
+    }
+
+    private void runConcurrently(int treads, int cycles, Runnable criticalSection) {
+        var barrier = new CyclicBarrier(treads);
+        var pool = Executors.newFixedThreadPool(treads);
+        IntStream.range(0, treads).forEach(i ->
+                pool.submit(() -> {
+                    try {
+                        barrier.await();
+                        IntStream.range(0, cycles).forEach(j -> criticalSection.run());
+                        barrier.await();
+                    } catch (InterruptedException | BrokenBarrierException e) {
+                        Assertions.fail(e);
                     }
-                    barrier.await();
-                } catch (InterruptedException | BrokenBarrierException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
+                }));
         pool.shutdown();
         Assertions.assertTimeout(Duration.ofSeconds(10),
                 () -> pool.awaitTermination(11, TimeUnit.SECONDS));
-        var duplicateElements = results.stream()
+    }
+
+    private List<Integer> collectDuplicates(List<Integer> results) {
+        return results.stream()
                 .filter(e -> Collections.frequency(results, e) > 1)
                 .distinct()
                 .collect(Collectors.toList());
-        Assertions.assertIterableEquals(Collections.emptyList(), duplicateElements);
     }
+
+    // TODO BoundedBuffer ??
+    // TODO volatile Incrementor?
+    // TODO better abstraction of test phases?
 
 }
